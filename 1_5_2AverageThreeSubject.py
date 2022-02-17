915f7869075dd4be555d3c2300fdad09d4ec6ba7
sahilgupta3030 <79680272+sahilgupta3030@users.noreply.github.com>
2022-02-17 13:16:55 +0530

    Delete 1_5_2AverageThreeSubject.py

--- 1_5_2AverageThreeSubject.py
+++
@@ -1,9 +0,0 @@
-eng_marks = int(input('Marks for English: '))
-maths_marks = int(input('Marks for Maths: '))
-social_marks = int(input('Marks for Social: '))
-
-average_marks = (eng_marks +
- maths_marks +
-  social_marks) / 3
-
-print('Average marks were: ',average_marks) 
